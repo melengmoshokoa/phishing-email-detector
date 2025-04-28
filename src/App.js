383f2657401cd4c64b36d2e8b4a333344611f709
melengmoshokoa <133795063+melengmoshokoa@users.noreply.github.com>
2025-04-28 14:56:48 +0200

    setting up the environment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './Pages/LandingPage'
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+     
+      <Route path="/" element={<LandingPage />} />
+
+    </Routes>
+  </Router>
+);
+}
+
+export default App;
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './LandingPage.css';  // This will be your custom CSS file
+
+function LandingPage() {
+    return (
+            <div className="Container"> 
+
+                <div className="Header"> 
+                </div>
+                    <dic className="Welcome">
+                        <h2>Hello welcome</h2>
+                        <p>Dont get caught in their nets</p>
+                    </dic>
+                    <div className="Buttons">
+                        <button>Check Your Email</button>
+                        <button>What is Phishing</button>
+                        <button>Why us?</button>
+                    </div>
+               
+            </div>
+    );
+}
+
+export default LandingPage;
